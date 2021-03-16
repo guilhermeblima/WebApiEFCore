f148b9ee12d5ce8ec6359da44411e0a1d86c4ad5
Guilherme Lima <guilhermelima.iba@gmail.com>
2021-03-16 18:21:30 +1300

    jwt implementation

---
+++ Repository/UserRepository.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+using ApiEFCore.Models;
+
+namespace ApiEFCore.Repository
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(new User {Id = 1, Username = "batman", Password = "batman", Role = "manager"});
+            users.Add(new User {Id = 1, Username = "robin", Password = "robin", Role = "employee"});
+            return (User)(users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == x.Password)).FirstOrDefault();
+        }
+    }
+    
+}
--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using ApiEFCore.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ApiEFCore
 {
@@ -31,7 +34,27 @@ namespace ApiEFCore
             services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
             // Add Context to app scope to prevent multiple connections
             services.AddScoped<DataContext, DataContext>();
+            services.AddCors();
             services.AddControllers();
+
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            services.AddAuthentication(x => 
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters 
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +69,14 @@ namespace ApiEFCore
 
             app.UseRouting();
 
+            //Cors added 
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            //Authentication added
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
